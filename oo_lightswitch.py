14c7f18954b64bd498815649251f5a9e25c84872
Cruz <cruzlopez.dev@gmail.com>
2024-02-06 18:01:02 -0600

    added example program

---
+++ oo_lightswitch.py
@@ -0,0 +1,17 @@
+class LightSwitch():
+    def __init__(self):
+        self.switchIsOn = False
+        
+    def turnOn(self):
+        self.switchIsOn = True
+        
+    def turnOff(self):
+        self.switchIsOn = False
+        
+    def show(self):
+        print(self.switchIsOn)
+        
+if __name__ == "__main__":
+    # instiate object here:
+    TODO
+    # use class methods here:
